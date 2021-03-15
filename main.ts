64bc301e5cb449e4e9b1b87af85dfb59c165963b
tbobula1 <80701974+tbobula1@users.noreply.github.com>
2021-03-15 11:23:09 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let set_hand_to = 0
+input.onGesture(Gesture.Shake, function () {
+    set_hand_to = randint(0, 2)
+    if (set_hand_to == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (set_hand_to == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
